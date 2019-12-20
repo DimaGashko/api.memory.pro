b3b950758a9441c1bd197754117c7d2ba0b3b478
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-20 02:38:59 +0200

    word model

--- app/Word.php
+++ app/Word.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $val
+ */
 class Word extends Model
 {
-    //
+    public $timestamps = false;
+
+    /** @var array */
+    protected $fillable = ['val'];
 }
