2f157f76ca1c390017f379726ecbb0032b93db6a
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-30 01:21:29 +0200

    fixed error with http resources on https server - another solution

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (config('app.env') === 'production') {
+        if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
             URL::forceScheme('https');
         }
     }
